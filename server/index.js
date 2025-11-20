a48d12b2c170e736f266336d20d30413520b2edd
Victoria Vega <victoria.vega247804@potros.itson.edu.mx>
2025-11-20 00:35:03 -0700

    Primer commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const http = require('node:http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.send(process.cwd()+'/cliente/index.html')
+});
+
+io.on('connection', (socket) => {
+    console.log('Un usuario se ha conectado.')
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    })
+});
+
+server.listen(3000, () => {
+    console.log('Escuchando en el puerto 3000')
+});
