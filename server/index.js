4ed71d24bcb3787ad9e5110e6e693bbd790df3f3
Victoria Vega <victoria.vega247804@potros.itson.edu.mx>
2025-11-20 01:08:18 -0700

    ya se pueden hacer chats privados y puedes mandarle mensaje a quien quieras

--- server/index.js
+++ server/index.js
@@ -6,14 +6,33 @@ const { Server } = require('socket.io');
 const io = new Server(server);
 
 app.get('/', (req, res) => {
-    res.sendFile(process.cwd()+'/cliente/index.html')
+    res.sendFile(process.cwd() + '/cliente/index.html')
 });
 
 io.on('connection', (socket) => {
-    console.log('Un usuario se ha conectado.')
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-    })
+    console.log('Usuario conectado:', socket.id);
+
+    const users = [];
+    for (let [id, socket] of io.of("/").sockets) {
+        users.push({ userID: id });
+    }
+    io.emit("users", users);
+
+    socket.on('private message', ({ content, to }) => {
+        socket.to(to).emit('private message', {
+            content,
+            from: socket.id
+        });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Usuario desconectado:', socket.id);
+        const remainingUsers = [];
+        for (let [id, socket] of io.of("/").sockets) {
+            remainingUsers.push({ userID: id });
+        }
+        io.emit("users", remainingUsers);
+    });
 });
 
 server.listen(3000, () => {
